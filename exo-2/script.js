d3d3e4f71d6b3eedd30e161ceadd27555065f593
Ezra Strikwerda <ezra@strikwerda.fr>
2023-05-23 14:37:20 +0200

    Ex 2 - 2 done

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,9 +1,20 @@
 
-const myDate = new Date();
 
+
+//1.
 let jourDeSemaine = "Nous sommes un jour de semaine";
 let jourDeWeekend = "Nous sommes un jour de weekend";
 
+//2.
+const myDate = new Date(2023, 4, 23);
+const day = myDate.getDay();
 
+console.log(day);
 
-console.log('exo-2');
+if (day == 0 || day == 6){
+    console.log(jourDeWeekend);
+}
+else {
+    console.log(jourDeSemaine)
+}
+
