ce422b95fce5e69f9c76be972aaa9c72b1f4ef0a
Ezra Strikwerda <ezra@strikwerda.fr>
2023-05-23 17:02:08 +0200

    Ex 3 - les fonctions - 1 donc

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -1,2 +1,16 @@
 // you can write js here
 console.log('exo-3');
+
+
+var playerInput = prompt("Your choice ( rock, paper, scissors) ?");
+
+console.log("yoyo");
+
+function getPlayerChoice(playerInput) {
+if (getPlayerChoice(playerInput) == rock || paper || scissors) {
+    console.log("ok");
+}
+else {
+    console.log("no");
+}
+}
