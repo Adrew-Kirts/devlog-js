4a6533192fe4884df39e0069f64b2429f6e6d6a7
Ezra Strikwerda <ezra@strikwerda.fr>
2023-05-23 12:04:14 +0200

    Ex 1 - Les variables

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,8 @@
-// you can write js here
-console.log('hello from file');
+
+
+var kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+let celsius = kelvins - 273; //la variable celsius à comme valeur; la valeur de kelvins - 273
+
+let fahrenheits = celsius * (1.8) + 32; //la variable fahrenheits à comme valeur; la valeur de celsius * 1.8 + 32
+
+console.log(Math.floor(fahrenheits)); //on affiche la valeur de la variable fahrenheits en utilisant Math.floor qui renvoie le plus grand entier qui est inférieur ou égal à un nombre 
