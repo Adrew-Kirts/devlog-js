f67ae866118de1a62f1038e91cef62895a897e50
Ezra Strikwerda <ezra@strikwerda.fr>
2023-05-24 15:13:22 +0200

    Exo 3 - finished

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -1,16 +1,68 @@
 // you can write js here
 console.log('exo-3');
 
+let playerInput = prompt("Your choice (rock, paper, scissors) ?");
+let choices = ["rock", "paper", "scissors"];
 
-var playerInput = prompt("Your choice ( rock, paper, scissors) ?");
-
-console.log("yoyo");
 
 function getPlayerChoice(playerInput) {
-if (getPlayerChoice(playerInput) == rock || paper || scissors) {
-    console.log("ok");
+
+    playerInput = playerInput.toLowerCase()
+
+    if (choices.includes(playerInput)) {
+        return playerInput;
+    }
+    else {
+        console.log("Please type rock, paper or scissors");
+    }
 }
-else {
-    console.log("no");
+
+function getComputerChoice() {
+
+    const number = Math.trunc(Math.random() * 3);
+    return choices[number];
 }
+
+function findWinner(playerChoice, computerChoice) {
+    console.log("Result:")
+    if (playerChoice == computerChoice) {
+        console.log("Tied");
+    }
+    else if (playerChoice == "rock" && computerChoice == "scissors") {
+        console.log("Won");
+    }
+    else if (playerChoice == "paper" && computerChoice == "rock") {
+        console.log("Won");
+    }
+    else if (playerChoice == "scissors" && computerChoice == "paper") {
+        console.log("Won");
+    }
+    else {
+        console.log("Lost");
+    }
+
 }
+
+function playGame() {
+    let uChoice = getPlayerChoice(playerInput);
+    console.log("You chose: ", uChoice);
+    let computerChoice = getComputerChoice();
+    console.log("Computer chose: ", computerChoice);
+    return findWinner(uChoice, computerChoice);
+
+}
+playGame();
+
+/*
+____________
+|Merci Alban|
+-------------
+const nb1 = 2
+const nb2 = 6
+
+let result = add(nb1, nb2);
+
+function add(a,b){
+    return a + b;
+}
+*/
