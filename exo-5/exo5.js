2cb0de056f3f2caadd45754e1f1d91a51b4bfe79
Ezra Strikwerda <ezra@strikwerda.fr>
2023-05-24 16:57:49 +0200

    Exo 5 - WIP

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,14 @@
 // you can write js here
 
 console.log('exo-5');
+
+let input = "phrase to translate";
+
+let vowels = ["a", "e", "i", "o", "u"];
+
+let resultArray = [];
+
+for (var i = 0; i < input.length; i++) {
+    console.log(input.charAt(i));
+  }
+  
