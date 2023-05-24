71d88b6a4778afadde1a5ee9fe90758370cbb5ec
Ezra Strikwerda <ezra@strikwerda.fr>
2023-05-24 16:22:37 +0200

    Exo 4 - finished

--- exo-4/script.js
+++ exo-4/script.js
@@ -2,4 +2,18 @@
 
 console.log('exo-4');
 
-var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+let secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+let poppedLastWord = secretMessage.pop();
+
+secretMessage.push("to", "learn");
+
+secretMessage[6] = "right";
+
+let poppedFirstword = secretMessage.shift();
+
+let newFirstWord = secretMessage.unshift("Programming");
+
+secretMessage.splice(5, 5, "know", "you");
+
+console.log(secretMessage.join());
